086bfa7345dbcb0a7d7fe99f7f0b6951b5af76eb
Shiwen Ai <shiwen.ai@gmail.com>
2024-07-20 19:25:39 -0700

    more rebranding changes

--- android/app/src/main/java/com/una/android/MainActivity.java
+++ android/app/src/main/java/com/una/android/MainActivity.java
@@ -10,16 +10,16 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate;
 public class MainActivity extends ReactActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this, R.style.SplashScreenTheme, true);
+/*SplashScreen.show(this, R.style.SplashScreenTheme); */       SplashScreen.show(this, R.style.SplashScreenTheme, true);
         super.onCreate(savedInstanceState);
-    }
+/*SplashScreen.show(this, R.style.SplashScreenTheme); */   }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
   @Override
   protected String getMainComponentName() {
-    return "una";
+    return "collaborito";
   }
 
   /**
